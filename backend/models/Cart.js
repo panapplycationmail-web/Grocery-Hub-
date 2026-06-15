2cdf0eda359f0fd9ae892022f5fc9c4329c877a6
panapplycationmail-web <panapplycationmail@gmail.com>
2026-06-15 14:57:14 +0530

    Add Cart model

---
+++ backend/models/Cart.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const cartSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  items: [{
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: 1
+    },
+    price: Number,
+    _id: false
+  }],
+  subtotal: Number,
+  tax: Number,
+  deliveryFee: {
+    type: Number,
+    default: 50
+  },
+  total: Number,
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Cart', cartSchema);
